f9c40c79d0cebafdf2247fd61da6e7d3b425b545
朱快 <zhukuai@git.wjr.com>
2021-05-07 18:52:31 +0800

    x

---
+++ Assets/exercise/nav/nav/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform dest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = dest.position;
+            //new Vector3(0,1.5f,44);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
